f88751b1dcb28e555eb4c2a9572ed9f94cba71be
Abdurizo <103021939+abdurizo@users.noreply.github.com>
2026-04-13 17:57:45 +0500

    responsive 640

---
+++ src/app/components/accordion-feature-list/accordion-feature-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion-feature-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './accordion-feature-list.component.html',
+  styleUrl: './accordion-feature-list.component.css'
+})
+export class AccordionFeatureListComponent {
+
+}
--- src/app/pages/pricing-page/pricing-table/pricing-table.component.ts
+++ src/app/pages/pricing-page/pricing-table/pricing-table.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AccordionFeatureListComponent } from '../../../components/accordion-feature-list/accordion-feature-list.component';
 
 @Component({
   selector: 'app-pricing-table',
   standalone: true,
-  imports: [],
+  imports: [AccordionFeatureListComponent],
   templateUrl: './pricing-table.component.html',
   styleUrl: './pricing-table.component.css'
 })
