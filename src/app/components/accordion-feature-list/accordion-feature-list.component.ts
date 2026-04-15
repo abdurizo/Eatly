c346a0824d1f0a34e5b8bae61554d261ac783b80
Abdurizo <103021939+abdurizo@users.noreply.github.com>
2026-04-15 19:04:42 +0500

    d

--- src/app/components/accordion-feature-list/accordion-feature-list.component.ts
+++ src/app/components/accordion-feature-list/accordion-feature-list.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-accordion-feature-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './accordion-feature-list.component.html',
   styleUrl: './accordion-feature-list.component.css'
 })
 export class AccordionFeatureListComponent {
+ activeIndex: number | null = null;
 
+  toggle(index: number) {
+    this.activeIndex = this.activeIndex === index ? null : index;
+  }
 }
